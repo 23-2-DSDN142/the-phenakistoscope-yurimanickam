16e091ff0f320cd0a45c6a4ab2bc4f62466d7276
yuri <79948608+yurimanickam@users.noreply.github.com>
2023-09-08 12:44:29 +1200

    pba commit, real proj. ltr

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,6 @@
 const SLICE_COUNT = 10;
+var blobs = [];
+
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -6,19 +8,28 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+
+
+  for (var i = 0; i < 29; i++) {
+    var blob = new Blob(random(-400, 1000), random(-400, 1000), random(5, 15), color(random(100), random(255), random(255)));
+    blobs.push(blob);
+  }
+
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 10,10,50);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  //var layer1 = new PLayer(faces);
+  //layer1.mode( SWIRL(5) );
+  //layer1.set_boundary( 200, 1000 );
 
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+
+
+  var layer3 = new PLayer(lava);
+  layer3.mode( RING );
+  layer3.set_boundary( 0, 800 );
 }
 
 function faces(x, y, animation, pScope){
@@ -48,5 +59,78 @@ function squares(x, y, animation, pScope){
 
 }
 
+// define a class for the blobs
+class Blob {
+  constructor(x, y, r, c) {
+    this.x = x; // x position
+    this.y = y; // y position
+    this.r = r; // radius
+    this.c = c; // color
+    this.vy = random(-0.001, -1); // vertical velocity
+    this.ay = -0.001; // vertical acceleration
+  }
 
+  // update the position and velocity of the blob
+  update() {
+    this.y += this.vy;
+    this.vy += this.ay;
+    // if the blob reaches the top boundary, reset its position and velocity
+    if (this.y < -400 - this.r) {
+      this.y = 400 + this.r;
+      this.vy = random(-5, -1);
+    }
+  }
+
+  // draw the blob as an ellipse
+  draw() {
+    fill(this.c);
+    noStroke();
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+}
+
+function lava(x, y, animation, pScope){
+  
+  // update and draw each blob in the array
+  for (var i = 0; i < blobs.length; i++) {
+    blobs[i].update();
+    blobs[i].draw();
+  }
+
+}
 //temp.commmit
+function drawSpinWheel() {
+  translate(width / 2, height / 2);
+
+  angle -= spin;
+
+  fill(60, 60, 30);
+  ellipse(0, 0, width - 100, width - 10);
+
+  for (var i = 0; i < 35; i++) {
+    var x = cos(angle) * r * (i / 4);
+    var y = sin(angle) * r * (i / 4);
+
+    fill(21, 90, 100);
+    ellipse(x, y, 5 + i, 5 + i);
+    noFill();
+    stroke(100);
+
+    rotate(0.5);
+  }
+
+  fill(100);
+  ellipse(0, 0, 60, 60);
+}
+
+function setup() {
+  createCanvas(500, 500);
+  colorMode(HSB, 100);
+}
+
+function draw() {
+  background(50);
+
+  // Call the drawSpinWheel function on top of your existing code
+  drawSpinWheel();
+}
